2807c44702c63138867ad54c705a91099e3bbb82
Adhika Gunadarma <adhikagunadarma@gmail.com>
2020-08-20 13:18:18 +0700

    Initial Commit

---
+++ finaccel_kredivo_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  finaccel_kredivo_test
+//
+//  Created by Josephine Fransisca on 20/08/20.
+//  Copyright © 2020 Adhika gunadarma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
